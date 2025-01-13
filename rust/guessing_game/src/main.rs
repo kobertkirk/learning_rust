1b3f56254cfd6b3205c2dda8c5dce4d114208246
kobert <none@email.com>
2025-01-12 20:40:00 -0600

    main

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -7,14 +8,35 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret numberis: {}", secret_number);
+    loop{
+        println!("please input your guess:");
+        let mut guess = String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("please input your guess:");
-    let mut guess = String::new();
-    io::stdin()
-       .read_line(&mut guess)
-       .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                if guess.trim() == "quit".to_string() || guess.trim() == "exit".to_string() {
+                    println!("Exiting the game...");
+                    break;
+                } else {
+                    println!("Invalid input. Please enter a number or 'exit' to quit.");
+                    continue;
+                }
+            }
 
-    println!("You guessed : {}", guess);
+        };
 
+        println!("You guessed : {}", guess);
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You guessed correctly!");
+                break;},
+        }
+    }
 }
