d1a00a834a29791b38fa3cabd61e67657d70758b
kobert <none@email.com>
2025-01-14 19:36:35 -0600

    main

---
+++ 3.1 Variables/target/debug/deps/variables-471e861027a37ec1.d
@@ -0,0 +1,5 @@
+/home/kobert/learning_rust/3.1 Variables/target/debug/deps/variables-471e861027a37ec1: src/main.rs
+
+/home/kobert/learning_rust/3.1 Variables/target/debug/deps/variables-471e861027a37ec1.d: src/main.rs
+
+src/main.rs:
---
+++ 3.1 Variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/kobert/learning_rust/3.1\ Variables/target/debug/variables: /home/kobert/learning_rust/3.1\ Variables/src/main.rs
