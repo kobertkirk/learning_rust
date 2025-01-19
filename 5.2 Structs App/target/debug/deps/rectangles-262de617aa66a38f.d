96565cca222b18d5f5f252e8a4387cc24cf066a2
kobert <none@email.com>
2025-01-18 21:15:55 -0600

    main

---
+++ 5.2 Structs App/target/debug/deps/rectangles-262de617aa66a38f.d
@@ -0,0 +1,5 @@
+/home/kobert/learning_rust/5.2 Structs App/target/debug/deps/rectangles-262de617aa66a38f: src/main.rs
+
+/home/kobert/learning_rust/5.2 Structs App/target/debug/deps/rectangles-262de617aa66a38f.d: src/main.rs
+
+src/main.rs:
---
+++ 5.2 Structs App/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/kobert/learning_rust/5.2\ Structs\ App/target/debug/rectangles: /home/kobert/learning_rust/5.2\ Structs\ App/src/main.rs
